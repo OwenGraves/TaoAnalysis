61cd5d17e533970320dd0291028174c4bca0436c
OwenGraves <ograves.owen@gmail.com>
2017-09-15 12:48:01 -0500

    finished ax3_2

--- chapter3.lean
+++ chapter3.lean
@@ -4,13 +4,12 @@ open nat
 open classical
 open set
 
-
 -- Since sets are not exactly straightforward in type theory,
 -- there will be some leniency with regards to the textbook
 -- in order to define them type theoretically and
 -- Lean's standard library will be employed
 -- to express the properties of sets.
-variable α : Type
+variable {α : Type}
 
 -- "set" will be used instead
 def d3_1_1 := α → Prop
@@ -18,7 +17,7 @@ def d3_1_1 := α → Prop
 -- sets are objects
 theorem ax3_1 (A : set α) (B : set (set α)) : Prop := A ∈ B
 
-def d3_1_4 (A B : set α) : A = B ↔ (∀ x : α, x ∈ A ↔ x ∈ B) :=
+def d3_1_4 {A B : set α} : A = B ↔ (∀ x : α, x ∈ A ↔ x ∈ B) :=
 begin
     constructor,
     intro h,
@@ -33,7 +32,15 @@ end
 
 theorem ax3_2 (A : set α) : (∀ x : α, x ∉ A) → A = ∅ :=  
 begin
-    intro h,
+    assume h,
+    apply iff.mpr d3_1_4,
+    assume x,
+    constructor,
+    assume xa,
+    have : x ∉ A, exact h x,
+    contradiction,
+    assume xe,
+    exact false.elim xe
 end
 
 
