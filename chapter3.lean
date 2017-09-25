439914edcf6859f7b72a840794fe8cf53fe2fc73
OwenGraves <ograves.owen@gmail.com>
2017-09-25 16:26:37 -0500

    changed assume to intro for consistency

--- chapter3.lean
+++ chapter3.lean
@@ -32,21 +32,21 @@ end
 
 theorem ax3_2 {A : set α} : (∀ x : α, x ∉ A) → A = ∅ :=  
 begin
-    assume h,
+    intro h,
     apply iff.mpr d3_1_4,
-    assume x,
+    intro x,
     constructor,
-    assume xa,
+    intro xa,
     have : x ∉ A, exact h x,
     contradiction,
-    assume xe,
+    intro xe,
     exact false.elim xe
 end
 
 lemma l3_1_6 {A : set α} (h : A ≠ ∅) : ∃ x, x ∈ A :=
 begin
     apply by_contradiction,
-    assume nh,
+    intro nh,
     have : ∀ x, x ∉ A, from forall_not_of_not_exists nh,
     have : A = ∅, from ax3_2 this,
     contradiction
@@ -55,12 +55,12 @@ end
 theorem ax3_3 {y : α} {a : α} : y ∈ ({a} : set α) ↔ y = a :=
 begin
     constructor,
-    assume ye,
+    intro ye,
         apply or.elim ye,
         exact id,
         assume (h : set.mem y ∅),
         exact false.elim h,
-    assume ye,
+    intro ye,
         exact or.inl ye
 end
 
